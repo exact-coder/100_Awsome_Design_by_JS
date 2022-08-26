5ac4bd9cf4ee583e73468d42a3aad452598fb156
Programming Bees <hasanakash799@gmail.com>
2022-08-27 04:11:51 +0600

    Drawing Aurora and Rainbow is complete

--- js/Aurora.js
+++ js/Aurora.js
@@ -2,6 +2,11 @@ class Aurora extends Project{
     // This is our super constructor.We call our every method in this constructor
     constructor(canvas){
         super(canvas)
+        this.step=0;
+        this.shape=[];
+        this.shape.push([-CANVAS_SIZE*0.1,CANVAS_SIZE*0.5]);
+        this.shape.push([CANVAS_SIZE*0.5,CANVAS_SIZE*0.2]);
+        this.shape.push([CANVAS_SIZE*1.1,CANVAS_SIZE*0.5]);
         this.drawFrame();
         this.showDisabled();
     }
@@ -9,19 +14,26 @@ class Aurora extends Project{
     //This drawFrame method is used setInterval function for blinking the flame of cloud
     drawFrame(){
         drawColoredBackground(this.ctx,"rgb(0,0,100)");
-        this.drawMoon([CANVAS_SIZE*0.5,CANVAS_SIZE*0.5]);
+        
+        this.drawAurora(this.shape);
+        this.step+=0.03;
+        this.shape[2][1]+=Math.sin(this.step+Math.PI);
+        this.shape[1][1]+=Math.sin(this.step);
+        this.shape[0][1]+=Math.cos(this.step);
     }
 
-    drawMoon(location){
-        this.ctx.beginPath();
-        this.ctx.fillStyle="yellow";
-        this.ctx.arc(location[0],location[1],CANVAS_SIZE*0.2,0,Math.PI*2);
-        this.ctx.fill();
-
-        this.ctx.beginPath();
-        this.ctx.fillStyle="rgb(0,0,100)";
-        this.ctx.arc(location[0]-CANVAS_SIZE*0.1,location[1],CANVAS_SIZE*0.2,0,Math.PI*2);
-        this.ctx.fill();
+    drawAurora(shape){
+        for(let i=0; i<=30;i++){
+            const dif = CANVAS_SIZE*0.2*(i/30);
+            this.ctx.beginPath();
+            this.ctx.moveTo(shape[0][0],shape[0][1]-dif*0.4);
+            this.ctx.strokeStyle="rgba(0,255,0,0.03)";
+            this.ctx.lineWidth=dif;
+            this.ctx.quadraticCurveTo(
+                shape[1][0],shape[1][1]-dif*0.4,
+                shape[2][0],shape[2][1]-dif*0.4);
+            this.ctx.stroke();
+        }
     }
 }
 
---
+++ js/Rainbow.js
@@ -0,0 +1,53 @@
+class Rainbow extends Project{
+    // This is our super constructor.We call our every method in this constructor
+    constructor(canvas){
+        super(canvas)
+        this.step=0;
+        this.shape=[];
+        this.shape.push([-CANVAS_SIZE*0.1,CANVAS_SIZE*0.5]);
+        this.shape.push([CANVAS_SIZE*0.5,CANVAS_SIZE*0.2]);
+        this.shape.push([CANVAS_SIZE*1.1,CANVAS_SIZE*0.5]);
+        this.drawFrame();
+        this.showDisabled();
+        // this.getColor();
+    }
+
+    //This drawFrame method is used setInterval function for blinking the flame of cloud
+    drawFrame(){
+        drawColoredBackground(this.ctx,"rgb(0,0,100)");
+        this.drawRainbow(this.shape);
+    }
+
+    getColor(index){
+        switch (index) {
+            case 0:"red";break;
+            case 1:"blue";break;
+            case 2:"orange";break;
+            case 4:"yellow";break;
+            case 5:"green";break;
+            case 6:"orange";break;
+            case 7:"coral";break;
+            default:"indigo"; break;
+        }
+    }
+
+    drawRainbow(shape){
+        for(let i=7; i >= 1;i--){
+            const dif = CANVAS_SIZE*0.2*(i/7);
+            this.ctx.beginPath();
+            this.ctx.moveTo(shape[0][0],shape[0][1]-dif*0.5);
+            this.ctx.strokeStyle=this.getColor(i);
+            console.log(this.getColor(i));
+            console.log(i);
+            this.ctx.lineWidth=dif;
+            this.ctx.quadraticCurveTo(
+                shape[1][0],shape[1][1]-dif*0.5,
+                shape[2][0],shape[2][1]-dif*0.5);
+            this.ctx.stroke();
+        }
+    }
+}
+
+
+
+// length is 3.12min
